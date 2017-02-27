0046bdde3184fc06eaab8d108abdd7dd65b317b6
koyuta <koyuuta5330@gmail.com>
2017-02-27 15:03:50 +0900

    制約どおりかしっかり確認

--- spec/app/service_spec.rb
+++ spec/app/service_spec.rb
@@ -4,18 +4,14 @@ describe command('curl -Lk localhost/nginx_status -o /dev/null -w "%{http_code}\
 	its(:stdout) { should match /^200$/ }
 end
 
-describe command('curl localhost/ -o /dev/null -w "%{http_code}\n" -s') do
-	its(:stdout) { should match /^300$/ }
+describe command('curl -H "Host:dev.example.com" http://localhost -o /dev/null -w "%{http_code}\n" -s') do
+  its(:stdout) { should match /^301$/ }
 end
 
-describe command('curl -Lk localhost/ -o /dev/null -w "%{http_code}\n" -s') do
-	its(:stdout) { should match /^200$/ }
+describe command('curl -kL -H "Host:dev.example.com" http://localhost -o /dev/null -w "%{http_code}\n" -s') do
+  its(:stdout) { should match /^200$/ }
 end
 
-describe command('curl -Lk -H "Host:dev.example.com" https://localhost -o /dev/null -w "%{http_code}\n" -s') do
-  its(:stdout) { should match /^300$/ }
-end
-
-describe command('curl -Lk -H "Host:dev.example.com" https://localhost -o /dev/null -w "%{http_code}\n" -s') do
-  its(:stdout) { should match /^300$/ }
+describe command('curl -k -H "Host:dev.example.com" https://localhost -o /dev/null -w "%{http_code}\n" -s') do
+  its(:stdout) { should match /^200$/ }
 end
