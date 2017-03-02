51c9b40f255ca4a9c2d673738cfd5809bd8c04d6
koyuta <koyuuta5330@gmail.com>
2017-03-02 12:11:59 +0900

    共通設定をredisにも適応

---
+++ spec/redis/general_spec.rb
@@ -0,0 +1,16 @@
+require "spec_helper"
+
+describe file("/etc/ssh/sshd_config") do
+	its(:content) { should match /^[^#]*PermitRootLogin without-password/ }
+end
+
+%w(vim wget).each do |pkg|
+	describe command("which #{pkg}") do
+		its(:exit_status) { should eq 0 }
+	end
+end
+
+describe service("firewalld") do
+	it { should be_enabled }
+	it { should be_running }
+end
